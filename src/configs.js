42511d4f600f033042b45f4464e6b03b634331b6
Youssef El-Bakkouri <youssef.elbakkouri@prestatech.com>
2023-05-22 15:34:50 +0200

    new db

--- src/configs.js
+++ src/configs.js
@@ -20,13 +20,11 @@ export const dbs = {
     appId: "1:559601261397:web:a2e11c581866a4228b2ddc",
   },
   db3: {
-    apiKey: "AIzaSyBjZdnfVU6Gfd9mz_zeugFNBZqRBTvGbro",
-    authDomain: "ayoub-81259.firebaseapp.com",
-    databaseURL:
-      "https://ayoub-81259-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "ayoub-81259",
-    storageBucket: "ayoub-81259.appspot.com",
-    messagingSenderId: "559601261397",
-    appId: "1:559601261397:web:a2e11c581866a4228b2ddc",
+    apiKey: "AIzaSyALrq_pCzT9S-kCPggefFpsqlIgO-jjAAE",
+    authDomain: "ayoub-d8f1b.firebaseapp.com",
+    projectId: "ayoub-d8f1b",
+    storageBucket: "ayoub-d8f1b.appspot.com",
+    messagingSenderId: "218656684247",
+    appId: "1:218656684247:web:68aa090b77e3ee6a3d9892",
   },
 };
