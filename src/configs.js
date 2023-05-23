049f150ee97851e9b159cccb5d38e9ae06d7b1fb
Youssef El-Bakkouri <youssef.elbakkouri@prestatech.com>
2023-05-23 12:58:33 +0200

    add db4

--- src/configs.js
+++ src/configs.js
@@ -10,12 +10,12 @@ export const dbs = {
     appId: "1:559601261397:web:a2e11c581866a4228b2ddc",
   },
   db2: {
-    apiKey: "AIzaSyAKIDuxgLUTjcg9r0Lsu5ZLpPT9OQvlArA",
-    authDomain: "ayoub-cbe10.firebaseapp.com",
-    projectId: "ayoub-cbe10",
-    storageBucket: "ayoub-cbe10.appspot.com",
-    messagingSenderId: "341475894389",
-    appId: "1:341475894389:web:abf6b7a117306f714d043c",
+    apiKey: "AIzaSyDdE47f3GXGtbcmRSZiKZvUjFNYGT4iemk",
+    authDomain: "aaaa-1087a.firebaseapp.com",
+    projectId: "aaaa-1087a",
+    storageBucket: "aaaa-1087a.appspot.com",
+    messagingSenderId: "930061039239",
+    appId: "1:930061039239:web:45c152bd61c9ab9f162a22",
   },
   db3: {
     apiKey: "AIzaSyALrq_pCzT9S-kCPggefFpsqlIgO-jjAAE",
@@ -24,5 +24,13 @@ export const dbs = {
     storageBucket: "ayoub-d8f1b.appspot.com",
     messagingSenderId: "218656684247",
     appId: "1:218656684247:web:68aa090b77e3ee6a3d9892",
+  },
+  db4: {
+    apiKey: "AIzaSyBTttlU1jL7o8RObh9-qR415FEL3GpixeU",
+    authDomain: "sdfsdfds-48cfa.firebaseapp.com",
+    projectId: "sdfsdfds-48cfa",
+    storageBucket: "sdfsdfds-48cfa.appspot.com",
+    messagingSenderId: "932348875945",
+    appId: "1:932348875945:web:47a0c79f8eaca2c1d605b8",
   },
 };
